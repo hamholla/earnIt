4a0b9a2230c9ce93e5629028cc55533240635208
Ham Hollett <hamhollett@gmail.com>
2023-07-11 18:51:56 -0500

    initial commit, build out activities & list

---
+++ src/Activity.js
@@ -0,0 +1,27 @@
+function Activity({ activity, handleClick }) {
+  return (
+    <div
+      onClick={() => handleClick(activity)}
+      className="relative flex w-96 flex-col rounded-xl bg-white bg-clip-border text-gray-700 shadow-md m-4 hover:shadow-lg hover:bg-gray-100 cursor-pointer"
+    >
+      <div>
+        <img
+          src={`https://maps.googleapis.com/maps/api/staticmap?size=600x300&maptype=roadmap&path=enc:${activity.map.summary_polyline}&key=${process.env.REACT_APP_GOOGLE_MAPS_API_KEY}`}
+        />
+      </div>
+      <div className="p-6">
+        <h5 className="mb-2 block font-sans text-xl font-semibold leading-snug tracking-normal text-blue-gray-900 antialiased">
+          {activity.name}
+        </h5>
+        <p className="block font-sans text-base font-light leading-relaxed text-inherit antialiased">
+          <div>Distance: {(activity.distance / 1609.344).toFixed(1)} miles</div>
+          <div>
+            Average Speed: {(activity.average_speed * 2.2369363).toFixed(1)} mph
+          </div>
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default Activity;
---
+++ src/ActivityList.js
@@ -0,0 +1,80 @@
+import React, { useState, useEffect } from "react";
+import Activity from "./Activity";
+
+function ActivityList() {
+  // State
+  const [isLoading, setIsLoading] = useState(true);
+  const [activities, setActivities] = useState({});
+  //Strava Credentials
+  let clientID = process.env.REACT_APP_STRAVA_API_CLIENT_ID;
+  let clientSecret = process.env.REACT_APP_STRAVA_API_KEY;
+
+  // refresh token and call address
+  const refreshToken = "3afb1cd6e9099d1d198b1f515f35744175d48f20";
+  const callRefresh = `https://www.strava.com/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`;
+
+  // endpoint for read-all activities. temporary token is added in getActivities()
+  const callActivities = `https://www.strava.com/api/v3/athlete/activities?access_token=`;
+
+  // Use refresh token to get current access token
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await fetch(callRefresh, {
+        method: "POST",
+      }).then((res) => res.json());
+      getActivities(result.access_token);
+    };
+    fetchData();
+  }, [callRefresh]);
+
+  // use current access token to call all activities
+  const getActivities = (access) => {
+    // console.log(callActivities + access)
+    fetch(callActivities + access)
+      .then((res) => res.json())
+      .then(
+        (data) => setActivities(data),
+        setIsLoading((prev) => !prev)
+      )
+      .catch((e) => console.log(e));
+  };
+
+  const getSingleActivity = (access, id) => {
+    const callActivity = `https://www.strava.com/api/v3/activities/${id}?access_token=`;
+    fetch(callActivity + access)
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+      .catch((e) => console.log(e));
+  };
+
+  const handleActivitySelect = (activity) => {
+    console.log(activity);
+    getSingleActivity("3afb1cd6e9099d1d198b1f515f35744175d48f20", activity.id);
+  };
+
+  let renderedActivities = [];
+
+  if (isLoading) {
+    renderedActivities = <div>Loading...</div>;
+  } else if (activities.length > 0) {
+    renderedActivities = activities.map((activity) => {
+      return (
+        <Activity
+          key={activity.id}
+          activity={activity}
+          handleClick={handleActivitySelect}
+        />
+      );
+    });
+  } else {
+    renderedActivities = <div>No activities found</div>;
+  }
+
+  return (
+    <div className="container mx-auto flex flex-row flex-wrap justify-center">
+      {renderedActivities}
+    </div>
+  );
+}
+
+export default ActivityList;
