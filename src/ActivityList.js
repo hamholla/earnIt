06ac11447eb85050745419611414e2c319319018
Ham Hollett <hamhollett@gmail.com>
2023-07-12 08:25:45 -0500

    fixup single activity fetch

--- src/ActivityList.js
+++ src/ActivityList.js
@@ -2,35 +2,30 @@ import React, { useState, useEffect } from "react";
 import Activity from "./Activity";
 
 function ActivityList() {
-  // State
   const [isLoading, setIsLoading] = useState(true);
   const [activities, setActivities] = useState({});
   const [selectedActivity, setSelectedActivity] = useState(null);
-  //Strava Credentials
+
   let clientID = process.env.REACT_APP_STRAVA_API_CLIENT_ID;
   let clientSecret = process.env.REACT_APP_STRAVA_API_KEY;
 
-  // refresh token and call address
   const refreshToken = "3afb1cd6e9099d1d198b1f515f35744175d48f20";
   const callRefresh = `https://www.strava.com/oauth/token?client_id=${clientID}&client_secret=${clientSecret}&refresh_token=${refreshToken}&grant_type=refresh_token`;
 
-  // endpoint for read-all activities. temporary token is added in getActivities()
   const callActivities = `https://www.strava.com/api/v3/athlete/activities?access_token=`;
 
-  // Use refresh token to get current access token
-  useEffect(() => {
-    const fetchData = async () => {
-      const result = await fetch(callRefresh, {
-        method: "POST",
-      }).then((res) => res.json());
-      getActivities(result.access_token);
-    };
-    fetchData();
-  }, [callRefresh]);
+  const getAccessToken = async () => {
+    const result = await fetch(callRefresh, {
+      method: "POST",
+    }).then((res) => res.json());
+    return result.access_token;
+  };
 
-  // use current access token to call all activities
+  const accessToken = getAccessToken().then((accessToken) => {
+    return accessToken;
+  });
+
   const getActivities = (access) => {
-    // console.log(callActivities + access)
     fetch(callActivities + access)
       .then((res) => res.json())
       .then(
@@ -49,10 +44,20 @@ function ActivityList() {
   };
 
   const handleActivitySelect = (activity) => {
-    console.log(selectedActivity);
-    getSingleActivity(refreshToken, activity.id);
-    console.log(selectedActivity);
+    const fetchActivityData = async () => {
+      getSingleActivity(await accessToken, activity.id);
+    };
+    fetchActivityData();
   };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      getActivities(await accessToken);
+    };
+    fetchData();
+  }, [callRefresh]);
+
+  console.log(selectedActivity);
 
   let renderedActivities = [];
 
--- src/App.js
+++ src/App.js
@@ -3,8 +3,13 @@ import ActivityList from "./ActivityList";
 
 function App() {
   return (
-    <div className="">
-      <ActivityList />
+    <div>
+      <div className="container mx-auto flex flex-row flex-wrap justify-center">
+        {/* <h1 className="mb-6 text-3xl">
+          Select an activity to see how many beers youve earned!
+        </h1> */}
+        <ActivityList />
+      </div>
     </div>
   );
 }
